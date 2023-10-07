5b10ac93d50e5f30cb95fb033d9878bff3466af8
thomas0031 <mail@mail>
2023-10-07 20:37:14 +0300

    It works on my machine.

--- include/foo.h
+++ include/foo.h
@@ -1,4 +1,13 @@
 #ifndef FOO_H
 #define FOO_H
 
+typedef struct foo {
+    int (*get)(void);
+    void (*set)(int);
+} foo;
+
+typedef foo *foo_t;
+
+foo_t foo_new(void);
+
 #endif // !FOO_H
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "include/foo.h"
 
+#include <stdio.h>
+
 int main(void)
 {
+    foo_t foo = foo_new();
+
+    printf("foo->get() = %d\n", foo->get());
+    foo->set(42);
+    printf("foo->get() = %d\n", foo->get());
+
     return 0;
 }
